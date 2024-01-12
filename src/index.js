7112de54df13a1fe03dc51d3f61d8cf373812abe
safurutani <bluesara24@gmail.com>
2024-01-12 10:48:55 -0800

    used strictmode when rendering

--- src/index.js
+++ src/index.js
@@ -24,4 +24,9 @@ export default function App() {
 }
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
